87169c1635d96d7dcea836e686a7f5b0c2159d77
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-10-31 03:04:38 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -z "`ls /etc/nginx`" ] 
-then
-	cp -R /etc-start/nginx/* /etc/nginx
-fi
+if [ ! -f "/etc/nginx/nginx.conf" ]; then cp -R -f /etc-start/nginx/* /etc/nginx; fi
 
-nginx
-#exec "$@"
+exec "$@"
