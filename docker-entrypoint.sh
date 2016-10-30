af5d2169fd8030486cef8cd2e3b7208cff4e7317
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-10-30 20:10:31 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -z "`ls /etc/nginx`" ] 
+then
+	cp -R /etc-start/nginx/ /etc/nginx
+fi
+
 if [ -z "`ls /etc/nginx/sites-enabled`" ] 
 then
 	cp -R /etc-start/nginx/sites-enabled/ /etc/nginx/sites-enabled
