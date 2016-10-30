07503df9a683bf9cbcbb614b15f708d2e3333687
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-10-30 22:33:11 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,17 +6,4 @@ then
 	cp -R /etc-start/nginx/ /etc/nginx
 fi
 
-if [ -z "`ls /etc/nginx/sites-enabled`" ] 
-then
-	cp -R /etc-start/nginx/sites-enabled/ /etc/nginx/sites-enabled
-fi
-if [ -z "`ls /etc/nginx/certs`" ] 
-then
-	cp -R /etc-start/nginx/certs/ /etc/nginx/certs
-fi
-if [ -z "`ls /etc/nginx/conf.d`" ] 
-then
-	cp -R /etc-start/nginx/conf.d/ /etc/nginx/conf.d
-fi
-
 exec "$@"
