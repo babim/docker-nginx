39085d517364cd22f71f53dad4d35dbce373fd2a
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-10 18:52:12 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,17 @@ set -e
 
 if [ ! -f "/etc/nginx/nginx.conf" ]; then cp -R -f /etc-start/nginx/* /etc/nginx; fi
 
+# set ID docker run
+agid=${agid:-$auid}
+auser=www-data
+
+if [[ -z "${auid}" ]]; then
+  echo "start"
+elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
+ echo "can't run in Root user. Default user still run."
+else
+  usermod -u $auid $auser
+  groupmod -g $agid $auser
+fi
+
 exec "$@"
