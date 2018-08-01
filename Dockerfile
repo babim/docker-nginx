92cdecee6da3c7b3ea4826b000532e63500cca78
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-08-02 03:07:51 +0700

    fix some error

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,30 @@
-FROM babim/debianbase
+FROM babim/ubuntubase
+
+# Download option
+## ubuntu/debian
+RUN apt-get update && \
+    apt-get install -y wget bash && cd / && wget --no-check-certificate https://raw.githubusercontent.com/babim/docker-tag-options/master/z%20SCRIPT%20AUTO/option.sh && \
+    chmod 755 /option.sh && apt-get purge -y wget
 
 RUN nginx=stable && \
-	echo 'deb http://nginx.org/packages/debian/ jessie nginx' > /etc/apt/sources.list.d/nginx-$nginx.list \
-	&& echo 'deb-src http://nginx.org/packages/debian/ jessie nginx' >> /etc/apt/sources.list.d/nginx-$nginx.list \
-	&& wget http://nginx.org/keys/nginx_signing.key -O- |apt-key add - \
-	&& apt-get update \
-	&& apt-get install -y nginx --force-yes \
-	&& rm -rf /var/lib/apt/lists/**
+    echo "deb http://ppa.launchpad.net/nginx/$nginx/ubuntu xenial main" > /etc/apt/sources.list.d/nginx-$nginx.list && \
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C && \
+    apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install software-properties-common -yq && add-apt-repository ppa:ondrej/php -y && \
+    apt-get update && apt-get install -y --force-yes nginx && \
+    chown -R www-data:www-data /var/lib/nginx && \
+    apt-get purge -y apache*
+
+# Fix run suck
+RUN mkdir -p /run/php/
+
+RUN apt-get clean && \
+    apt-get autoclean && \
+    apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/**
 
 # Define mountable directories.
-VOLUME ["/etc/nginx", "/var/log/nginx", "/var/www/"]
+VOLUME ["/etc/nginx", "/var/log/nginx", "/var/www", "/etc/php"]
 
 # Define working directory.
 WORKDIR /etc/nginx
@@ -17,12 +32,24 @@ WORKDIR /etc/nginx
 # Copy include file
 COPY include /etc/nginx/include
 
-RUN mkdir -p /etc-start/nginx \
-	&& cp -R /etc/nginx/* /etc-start/nginx
-	
+# create folder    
+RUN [ -d /var/cache/nginx ] || mkdir -p /var/cache/nginx && \
+    [ -d /var/log/nginx ] || mkdir -p /var/log/nginx
+
+# prepare etc start
+RUN [ -d /etc/nginx ] || mkdir -p /etc-start/nginx && \
+    [ -d /etc/nginx ] || cp -R /etc/nginx/* /etc-start/nginx && \
+    [ -d /etc/php ] || mkdir -p /etc-start/nginx /etc-start/php && \
+    [ -d /etc/php ] || cp -R /etc/php/* /etc-start/php && \
+    [ -d /etc/apache2 ] || mkdir -p /etc-start/apache2 && \
+    [ -d /etc/apache2 ] || cp -R /etc/apache2/* /etc-start/apache2 && \
+    [ -d /var/www ] || mkdir -p /etc-start/www && \
+    [ -d /var/www ] || cp -R /var/www/* /etc-start/www
+
 COPY docker-entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
+CMD ["nginx", "-g", "'daemon off;'"]
 
 # forward request and error logs to docker log collector
 RUN ln -sf /dev/stdout /var/log/nginx/access.log
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,65 @@
 #!/bin/bash
 export TERM=xterm
 
+# option with entrypoint
+if [ -f "/option.sh" ]; then /option.sh; fi
+
 if [ ! -f "/etc/nginx/nginx.conf" ]; then cp -R -f /etc-start/nginx/* /etc/nginx; fi
+
+if [ -d "/etc/php" ]; then
+if [ -z "`ls /etc/php`" ]; then 
+	cp -R /etc-start/php/* /etc/php
+
+ # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	# set php value
+	for VARIABLE in /etc/php/*
+	do
+	if [ -f "$VARIABLE/php.ini" ]; then
+	sed -i -E \
+		-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
+		-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
+		-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
+		-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+		-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+		-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+		-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+		-e "s/error_reporting = .*/error_reporting = E_ALL/" \
+		-e "s/display_errors = .*/display_errors = On/" \
+	$VARIABLE/php.ini
+	fi
+	done
+
+	# set opcache
+	for VARIABLE in /etc/php/*
+	do
+	if [ -f "$VARIABLE/php.ini" ]; then
+	sed -i -E \
+		-e "s|^;*\(opcache.enable\) *=.*|\1 = 1|" \
+		-e "s|^;*\(opcache.enable_cli\) *=.*|\1 = 1|" \
+		-e "s|^;*\(opcache.fast_shutdown\) *=.*|\1 = 1|" \
+		-e "s|^;*\(opcache.interned_strings_buffer\) *=.*|\1 = 8|" \
+		-e "s|^;*\(opcache.max_accelerated_files\) *=.*|\1 = 4000|" \
+		-e "s|^;*\(opcache.memory_consumption\) *=.*|\1 = 128|" \
+		-e "s|^;*\(opcache.revalidate_freq\) *=.*|\1 = 60|" \
+	$VARIABLE/php.ini
+	fi
+	done
+fi
+fi
+if [ -d "/var/www" ]; then
+if [ -z "`ls /var/www`" ]; then
+	cp -R /etc-start/www/* /var/www
+	chown -R www-data:www-data /var/www
+fi
+fi
 
 # set ID docker run
 agid=${agid:-$auid}
@@ -16,10 +74,21 @@ if [[ -z "${auid}" ]]; then
 elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
 	echo "run in user root"
 	auser=root
-	#export APACHE_RUN_USER=$auser
-	#export APACHE_RUN_GROUP=$auser
-	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#Set php user
+if [ -d "/etc/php" ]; then
+if [ -z "`ls /etc/php`" ]; then 
+	for VARIABLE in /etc/php/*
+	do
+	if [ -f "$VARIABLE/fpm/php-fpm.conf" ]; then
+	sed -i -E \
+		-e "/^user = .*/cuser = $auser" \
+		-e "/^group = .*/cgroup = $auser" \
+	$VARIABLE/fpm/php-fpm.conf
+	fi
+	done
+fi
+fi
+	#Set nginx user
 	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 elif id $auid >/dev/null 2>&1; then
@@ -27,8 +96,20 @@ elif id $auid >/dev/null 2>&1; then
 else
 if id $auser >/dev/null 2>&1; then
         echo "user exists"
-	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#Set php user
+if [ -d "/etc/php" ]; then
+if [ -z "`ls /etc/php`" ]; then 
+	for VARIABLE in /etc/php/*
+	do
+	if [ -f "$VARIABLE/fpm/php-fpm.conf" ]; then
+	sed -i -E \
+		-e "/^user = .*/cuser = $auser" \
+		-e "/^group = .*/cgroup = $auser" \
+	$VARIABLE/fpm/php-fpm.conf
+	fi
+	done
+fi
+fi
 	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	#export APACHE_RUN_USER=$auser
@@ -47,19 +128,28 @@ else
 	#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
 	# create user ubuntu/debian
 	groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
-	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#Set php user
+if [ -d "/etc/php" ]; then
+if [ -z "`ls /etc/php`" ]; then 
+	for VARIABLE in /etc/php/*
+	do
+	if [ -f "$VARIABLE/fpm/php-fpm.conf" ]; then
+	sed -i -E \
+		-e "/^user = .*/cuser = $auser" \
+		-e "/^group = .*/cgroup = $auser" \
+	$VARIABLE/fpm/php-fpm.conf
+	fi
+	done
+fi
+fi
 	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 fi
 
 fi
 
-# ssh
-if [ -f "/runssh.sh" ]; then /runssh.sh; fi
-# cron
-if [ -f "/runcron.sh" ]; then /runcron.sh; fi
-# nfs
-if [ -f "/mountnfs.sh" ]; then /mountnfs.sh; fi
-
-nginx -g 'daemon off;'
+# run PHP-fpm
+if [ -f "/usr/bin/php-fpm5.6" ]; then php-fpm5.6 -D; fi
+if [ -f "/usr/bin/php-fpm7.0" ]; then php-fpm7.0 -D; fi
+if [ -f "/usr/bin/php-fpm7.1" ]; then php-fpm7.1 -D; fi
+if [ -f "/usr/bin/php-fpm7.2" ]; then php-fpm7.2 -D; fi
